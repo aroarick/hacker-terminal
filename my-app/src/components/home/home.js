7492d4abb7bb11128342008a3938373a66c95e62
Ashley Roarick <ahorseroar@gmail.com>
2020-11-27 11:21:32 -0600

    installed bootstrap

---
+++ my-app/src/components/home/home.js
@@ -0,0 +1,15 @@
+import React from "react";
+// import Row from "react-bootstrap/Row";
+
+
+import "../../App.css";
+
+function home() {
+  return (
+    <>
+      <h1>hi</h1>
+    </>
+  );
+}
+
+export default home;
